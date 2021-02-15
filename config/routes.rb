761e5f0609cff8f0b31ab3bff3a68dfce659768e
Andres Puello <andres.puello@symplifica.com>
2021-02-15 16:48:32 -0500

    Login

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,14 @@ Rails.application.routes.draw do
   resources :posts
   resources :followers
   devise_for :users
-  root 'home#index'
+  devise_scope :user do
+    authenticated :user do
+      root 'home#index', as: :authenticated_root
+    end
+  
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
